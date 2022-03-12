876bfbcf4a82195cad46766a4366f8a786ce2e6d
moonsunearth98 <prachibaranwal27@gmail.com>
2022-03-12 15:29:32 +0530

    My Portfolio Sample

---
+++ pr.js/prachi.js
@@ -0,0 +1,10 @@
+threelines = document.querySelector('.threelines')
+header = document.querySelector('.header')
+navlist = document.querySelector('.navlist')
+
+threelines.addEventListener('click', ()=>{
+    header.classList.toggle('v-class')
+    navlisy.classList.toggle('v-class')
+    
+
+})
